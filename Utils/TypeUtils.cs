b23dc948207c25f7f5cc58f783cd87373af42872
spladison@gmail.com <spladison@gmail.com>
2023-08-06 15:56:55 +0300

    SetFieldAccessor<>() in TypeUtils

--- Utils/TypeUtils.cs
+++ Utils/TypeUtils.cs
@@ -5,13 +5,24 @@ namespace BetterExpertRarity.Utils
 {
     public static class TypeUtils
     {
-        public static Func<T, R> GetFieldAccessor<T, R>(string fieldName)
+        public static Func<T, V> GetFieldAccessor<T, V>(string fieldName)
         {
             var param = Expression.Parameter(typeof(T), "arg");
             var member = Expression.Field(param, fieldName);
-            var lambda = Expression.Lambda(typeof(Func<T, R>), member, param);
+            var lambda = Expression.Lambda(typeof(Func<T, V>), member, param);
 
-            return (Func<T, R>)lambda.Compile();
+            return lambda.Compile() as Func<T, V>;
+        }
+
+        public static Action<T, V> SetFieldAccessor<T, V>(string fieldName)
+        {
+            var param = Expression.Parameter(typeof(T), "arg");
+            var valueParam = Expression.Parameter(typeof(V), "value");
+            var member = Expression.Field(param, fieldName);
+            var assign = Expression.Assign(member, valueParam);
+            var lambda = Expression.Lambda(typeof(Action<T, V>), assign, param, valueParam);
+
+            return lambda.Compile() as Action<T, V>;
         }
     }
 }
